2c345705c044d711682d1c29d36065b4fcffa722
Arthur Ringoringo <42938790+arthurringoringo@users.noreply.github.com>
2021-02-12 18:57:46 +0700

    Add project files.

---
+++ Infrastructure/DataContext/APIDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using APIACMS.Infrastructure.Models;
+
+namespace APIACMS.Infrastructure.DataContext
+{
+    public class APIDbContext : DbContext
+    {
+
+
+        public DbSet<AvailableClass> AvailableClasses { get; set; }
+        public DbSet<ClassCategory> ClassCategories { get; set; }
+        public DbSet<ClassReport> ClassReports { get; set; }
+        public DbSet<PaidSession> PaidSessions { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<RegistredClass> RegistredClasses { get; set; }
+        public DbSet<SessionSchedule> SessionSchedules { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options) { 
+        }
+        public APIDbContext NewInstance()
+        {
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<APIDbContext>();
+            dbContextOptionsBuilder.UseSqlServer(this.Database.GetDbConnection().ConnectionString);
+
+            return new APIDbContext(dbContextOptionsBuilder.Options);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIACMS.Infrastructure.DataContext;
+using APIACMS.Infrastructure.DbExtension;
+using System.Reflection;
+
+namespace APIACMS
+{
+    public class Startup
+    {//private const string AllowOrigins = "*";
+        readonly string MyPolicyName = "_MyPolicyName";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //TODO add Db Context
+            services.AddDbContext<APIDbContext>(
+                   e =>
+                   {
+                       e.EnableSensitiveDataLogging();
+                       e.UseSqlServer(Configuration["MSSQL_CONNECTIONSTRING"],
+                       sqlOptions =>
+                       {
+                           sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                           sqlOptions.EnableRetryOnFailure(10, TimeSpan.FromSeconds(30), null);
+                       });
+
+                   });
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyPolicyName,
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
+
+
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //AutomigrateDatabase Disabled for now
+            //app.UpdateDatabase();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(MyPolicyName);
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+            });
+        }
+    }
+}
